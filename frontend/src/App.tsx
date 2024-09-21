feda9ae7ac6e7cac9bb5adc6ff54bd14930d9be0
mohit <mohitsasane76@gmail.com>
2024-09-21 13:12:12 +0530

    all work done just styling remaining

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,33 +6,51 @@ import Salary from './Components/Salary'
 import SalaryTable from './Components/SalaryTable'
 
 
+import { ToastContainer, toast } from 'react-toastify';
 
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  
+
+
+
 
   const [value,setValue]=useState<string|null>(null);
   const [isActive,SetIsActive]=useState<boolean>(false);
 
   function clickFunction(text:string){
+    
+
 
     if(value===null){
 
       SetIsActive(prev=>!prev)
+      toast.info("Table appear");
+
     } 
     setValue(text)
+
+    toast.success("Table Updated");
+ 
    
+    
   }
   console.log(value)
- 
+
+  
 
   return (
     <>
     
 
+    <ToastContainer
+    position="top-center"
+    />
 
     <div className='flex items-center justify-around h-screen m-5'>
 
-
+    
 
     <Salary clickFunction={clickFunction} value={value} />
 
@@ -43,6 +61,7 @@ function App() {
 
     <div className='mt-20'>
     <Checkline/>
+    
     </div>
     </>
   )
